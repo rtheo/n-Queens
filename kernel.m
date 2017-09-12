769f870719990d1fc5372f1c804a0b6889672f4e
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-09-12 13:36:43 +0300

    Update kernel.m

--- kernel.m
+++ kernel.m
@@ -9,12 +9,10 @@ m3 = fliplr(m1);
 m4 = fliplr(m2);
 for i=1:dim
     for j=1:dim
-        w = zeros(dim); 
-        w(i, :) = 1; w(:, j) = 1;
+        w = zeros(dim); w(i, :) = 1; w(:, j) = 1;
         w = w + diag( ones( m2(i, j), 1 ), m1(i, j) );
         w = w + fliplr( diag( ones( m4(i, j), 1), m3(i, j) ) );
-        w(i, j) = 0;        
-        K = [K; reshape( w', 1, newdim )];
+        w(i, j) = 0; K = [K; reshape( w', 1, newdim )];
        % imagesc(1-w), title(['i = ',num2str(i),' j = ',num2str(j)]), colormap gray;pause
     end
 end
